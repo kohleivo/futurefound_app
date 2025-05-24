b5279c77974eee6b21b4802ebd40abc596e482d5
gawainweishaupt <gawain.weishaupt@gmail.com>
2025-05-24 11:45:24 +0200

    Create streamlit_app.py
    v1.0.0 Startseite

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="FutureFound App", page_icon="🚀")
+
+# Seite 1
+st.title("Willkommen!")
+st.write("""
+Du bist Business Consultant, Spezialistin für smarte Startups (also, fast… jedenfalls behauptet das dein LinkedIn-Profil).
+Eines Tages bekommst du eine Nachricht vom kleinen, innovativen Startup FutureFound, das kurz vor dem Aus steht.
+""")
