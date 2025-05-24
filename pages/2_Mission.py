a786a8ed168e325f78bcd776800cb8f5040deb63
gawainweishaupt <gawain.weishaupt@gmail.com>
2025-05-24 13:11:49 +0200

    Update 2_Mission.py

--- pages/2_Mission.py
+++ pages/2_Mission.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-# Zustand initialisieren
-if "mission_gestartet" not in st.session_state:
-    st.session_state["mission_gestartet"] = False
-
 st.title("Mission")
 st.write("Video 1:")
 
@@ -11,17 +7,8 @@ video_file = open("video 2.mp4", "rb")
 video_bytes = video_file.read()
 st.video(video_bytes)
 
-# Button-Callback-Funktion
-def mission_starten():
-    st.session_state["mission_gestartet"] = True
+col1, col2 = st.columns([1,1])
 
-# "Mission Starten"-Button, nur anzeigen, wenn Mission noch nicht gestartet
-if not st.session_state["mission_gestartet"]:
-    st.button("Mission Starten", on_click=mission_starten)
-
-# "Weiter" und "Zurück"-Buttons nur anzeigen, wenn Mission gestartet wurde
-if st.session_state["mission_gestartet"]:
-    col1, col2 = st.columns([1,1])
     with col1:
         if st.button("Zurück"):
             st.switch_page("streamlit_app.py")
