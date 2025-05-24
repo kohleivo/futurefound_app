524a8a76880684f5993d8ec324c4bb9a0ad3410c
gawainweishaupt <gawain.weishaupt@gmail.com>
2025-05-24 13:13:17 +0200

    Update 2_Mission.py

--- pages/2_Mission.py
+++ pages/2_Mission.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Mission")
-st.write("Video 1:")
+st.write("Video 2:")
 
 video_file = open("video 2.mp4", "rb")
 video_bytes = video_file.read()
@@ -9,9 +9,9 @@ st.video(video_bytes)
 
 col1, col2 = st.columns([1,1])
 
-    with col1:
-        if st.button("Zurück"):
-            st.switch_page("streamlit_app.py")
-    with col2:
-        if st.button("Weiter"):
-            st.switch_page("pages/3_Kapitel1.py")
+with col1:
+    if st.button("Zurück"):
+        st.switch_page("streamlit_app.py")
+with col2:
+    if st.button("Weiter"):
+        st.switch_page("pages/3_Kapitel1.py")
