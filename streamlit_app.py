446a86de8d256efa0f1e7a1baf504f0409576372
gawainweishaupt <gawain.weishaupt@gmail.com>
2025-05-24 13:09:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,9 @@ st.write("""
 Du bist Business Consultant, Spezialistin für smarte Startups (also, fast… jedenfalls behauptet das dein LinkedIn-Profil).
 Eines Tages bekommst du eine Nachricht vom kleinen, innovativen Startup FutureFound, das kurz vor dem Aus steht.
 """)
-if st.button("Weiter"):
+video_file = open("video 1.mp4", "rb")
+video_bytes = video_file.read()
+st.video(video_bytes)
+
+if st.button("Mission starten!"):
     st.switch_page("pages/2_Mission.py")
