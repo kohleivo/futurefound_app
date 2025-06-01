fa0aa8f3316d0ed47714656ae999034e67664ac9
gawainweishaupt <gawain.weishaupt@gmail.com>
2025-06-01 11:39:02 +0200

    Update 2_Kapitel1.py

--- pages/2_Kapitel1.py
+++ pages/2_Kapitel1.py
@@ -15,7 +15,7 @@ with open("video 2.mp4", "rb") as video_file:
     st.video(video_file.read())
     st.markdown(
     '<div style="font-size:0.95em; font-style:italic; color:#bfc9d1; text-align:left; margin-top:0.2em;">'
-    'Quelle: <a href="https://studyflix.de/wirtschaft-schueler/grunden-mit-dem-lean-startup-ansatz-546/video" style="color:#bfc9d1;" target="_blank">https://dein-link-zur-quelle.de</a>'
+    'Quelle: <a href="https://studyflix.de/wirtschaft-schueler/grunden-mit-dem-lean-startup-ansatz-546/video" style="color:#bfc9d1;" target="_blank">https://studyflix.de</a>'
     '</div>',
     unsafe_allow_html=True
 )
